ec1e9a3e547553c4612aa05390c62a4058e8d5e9
grason <grasonbrennan@gmail.com>
2013-11-12 11:56:17 -0500

    foo.py functions correctly

---
+++ foo.py
@@ -0,0 +1,9 @@
+import math
+def foo(num):
+    for i in range(30,-1,-2):
+        ander = int(math.pow(2,i)+math.pow(2,i+1))
+        yield (ander & num) >> i
+z = 0
+print('{:032b}'.format(2289985753))
+for k in foo(2289985753):
+    print('{:02b}'.format(k))
