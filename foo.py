021ff212e74201634f319cfdad435501b1ec1405
grason <grasonbrennan@gmail.com>
2013-11-18 18:51:04 -0500

    foo.py updated to deal with masks

--- foo.py
+++ foo.py
@@ -3,7 +3,27 @@ def foo(num):
     for i in range(30,-1,-2):
         ander = int(math.pow(2,i)+math.pow(2,i+1))
         yield (ander & num) >> i
-z = 0
-print('{:032b}'.format(2289985753))
-for k in foo(2289985753):
-    print(k)
+
+def genArray(i, mask):
+    a = foo(i)
+    b = []
+    h = int((mask / 2))
+    for k in a:
+        b.append(k)
+    if mask % 2:
+       
+        b = b[0:h+1]
+        b[h] = b[h] & 3
+        for k in b:
+            print('{:02b}'.format(k))
+        #create
+        b[h] = b[h] + 1
+        for k in b:
+            print('{:02b}'.format(k))
+        #create again
+    else:
+        print("even")
+        b = b[:h]
+        for k in b:
+            print('{:02b}'.format(k))
+genArray(16777216,16)        
